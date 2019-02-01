85b5b22271126b07bd49e4110b1aa6137cc9e5fd
lucasdlg5 <lucasdomingosleao55@hotmail.com>
2019-01-31 22:21:26 -0200

    Adicionando correcao alt-text

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -5,7 +5,7 @@ class Movie extends Component {
         return (
           <div className="movie">
               <h2>{this.props.title}</h2>
-              <div><img width="200" src={ this.props.poster}/></div>
+              <div><img width="200" src={ this.props.poster} alt=""/></div>
               <p>({this.props.year})</p>
               <p>{this.props.description}</p>
           </div>  
